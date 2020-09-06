97506c805d433c4f7eece04ad4a85e299722fc2a
Luis Nieves <nievesh.lf@gmail.com>
2020-09-06 13:00:15 -0700

    Updates to readme, provisioning, and requirements.

--- README.md
+++ README.md
@@ -2,10 +2,32 @@
 
 Repository for playing with a Jetson Nano. 
 
-# How to Build
 
-### Dependencies
+## Requirements
 
-See `requirements.txt` for Python dependencies. The Jetson Nano requires some packages to be installed for the Python
-program to run. See `provision.sh` for details. 
+## Hardware
+
+You will need the following hardware to run this project:
+
+* [Jetson Nano Developer Kit](https://developer.nvidia.com/embedded/jetson-nano-developer-kit)
+* [5V 4A power supply](https://www.amazon.com/gp/product/B07413Q5Y4/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07413Q5Y4&linkCode=as2&tag=desertbot-20&linkId=785912983fcb0207e5d0940ebfca9423)
+* J48 jumper
+* [Raspberry Pi Camera Module v2](https://www.digikey.com/catalog/en/partgroup/raspberry-pi-camera-module-v2/63181?utm_adgroup=Programmers%20Dev&utm_source=google&utm_medium=cpc&utm_campaign=Dynamic%20Search_RLSA_Buyers&utm_term=&utm_content=Programmers%20Dev&gclid=Cj0KCQjwhb36BRCfARIsAKcXh6H0b37_zqrGRSlMun8dv3EY0uEGLlyEkfF78_dqaqqi8Fb2kC6oz9IaAtW8EALw_wcB) or equivalent
+* Cat 5 or Wi-Fi connection
+
+## Software
+
+You will need the following software to run this project:
+
+* [JetPack SDK for Jetson Nano](https://developer.nvidia.com/embedded/jetpack)
+* Python packages listed in `requirements.txt` 
+
+### Installing and Running Application
+
+This program requires some other tools to be installed on the Jetson Nano. All of these can be installed
+by running `scripts/provision.sh` on the Jetson Nano. 
+
+This script will also create a systemd service which will automatically start the program upon boot and restart
+the program upon crash. The program may be disabled by running `sudo systemctl --user disable camera-nano.service`.
+
 
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -13,13 +13,49 @@ sudo apt full-upgrade -y
 
 echo 'Downloading packages.'
 
-sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+# Required packages for TensorFlow:
+sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran -y
 
-sudo apt-get install python3-pip
+# Install pip3
+sudo apt-get install python3-pip -y
+yes | sudo pip3 install -U pip testresources setuptools
 
-sudo pip3 install -U pip
+# Python package dependencies
+yes | sudo pip3 install -U pip testresources setuptools numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+# $ sudo pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
 
-sudo pip3 install -U pip testresources setuptools numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+# TF-2.2
+yes | sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow==2.2.0+nv20.7
 
-# TF-2.x
-$ sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow==2.2.0+nv20.7
+# TODO: Install Python packages
+yes | sudo pip3 install -r ../requirements.txt
+
+# Remove deprecated packages
+sudo apt auto-remove -y
+
+# Create the service
+echo 'Creating systemd service.'
+
+{
+  echo '[Unit]'
+  echo 'Description=Object detection service that streams on the network'
+  echo ''
+  echo '[Service]'
+  echo 'ExecStart=/usr/bin/camera_nano.py'
+  echo 'Restart=on-failure'
+  echo 'RestartSec=5'
+  echo 'User=jetson'
+  echo ''
+  echo '[Install]'
+  echo 'WantedBy=default.target'
+} | sudo tee /lib/systemd/system/camera-nano.service
+
+sudo chown root:root /lib/systemd/system/camera-nano.service
+sudo chmod 644 /lib/systemd/system/camera-nano.service
+sudo systemctl --user enable camera-nano.service
+
+sync
+
+echo
+echo 'Updates complete. Please reboot.'
+echo
